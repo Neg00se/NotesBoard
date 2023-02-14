5f15ff791d56d98b7e0c13ae6761bb23f12df71f
Neg00se <gooosb205@gmail.com>
2023-02-14 18:27:41 +0200

    Project Setup

---
+++ NotesApiClassLibrary/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApiClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApiClassLibrary.Data;
+public class ApplicationDbContext : DbContext
+{
+
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):
+		base(options)
+	{
+
+	}
+
+	public DbSet<NoteModel> Notes { get; set; }
+}
